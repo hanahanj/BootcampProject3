f0fee99319be1ba88a1dd684081a99c1bc08aa07
Zariah <zaripull18@gmail.com>
2024-06-03 20:55:52 -0500

    addeed .npmrc file

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcryptjs = require('bcryptjs');
 
 // import schema from Shirt.js
 const shirtSchema = require('./Shirt');
