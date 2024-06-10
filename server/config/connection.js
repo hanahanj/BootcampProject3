6ead5d71039d81750fd7274ff45e28d3e013caf6
Matthew Lowery <matthewdlowery@msn.com>
2024-06-10 10:11:02 -0500

    Changed name in package.json, added shirts-db to connection.js, and edited .env file

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/shirts-db');
 
 module.exports = mongoose.connection;
